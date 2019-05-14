a171b7d1d136d65e00fbcab00b2953da8f446ba5
Gabriel Forien <gforien@gmail.com>
2019-05-14 12:32:33 +0200

    :wrench: gitignore updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
 const path = require('path');
 
-
 module.exports = {
-  // context: __dirname,
+
   entry: "./public/lib/guitar_hero.js",
-  // target: 'node',
+
   output: {
     path: path.resolve(__dirname, 'public/build/'),
   	filename: "bundle.js"
   },
+
   mode: 'development'
-  // resolve: {
-  //   extensions: ['.js']
-  // },
-  //devtool: 'source-map',
-  //watch: true
 };
