7b4b775fda3186a887bf8c8d8c7f52c171cd9a2a
aballiet <antoine.balliet@gmail.com>
2020-03-18 16:58:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.get('/spotify', function(req, res) {
 app.get('/login', function(req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
-  var scope = 'user-read-recently-played user-top-read user-library-modify user-library-read playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-read-email user-read-birthdate user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-follow-read user-follow-modify';
+  var scope = 'user-read-recently-played user-top-read user-library-modify user-library-read playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-follow-read user-follow-modify';
 
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
