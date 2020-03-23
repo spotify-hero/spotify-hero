129df7e0655963b236125011dbace9d4d8a7e5dc
aballiet <antoine.balliet@gmail.com>
2020-03-24 00:51:43 +0100

    New UI with cards :sparkles::lipstick:
    - Search Bar (almost) working
    - Hidden scroll
    - Fix various graphic glitches

--- public/js/select.js
+++ public/js/select.js
@@ -10,50 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
   array.forEach((table)=> {
     addTrackHTMLBeforeElement('anchor', table);
   });
-  document.getElementById('searchTerm').onkeypress = (()=>{filterList('searchTerm', "track_table")});
-  document.getElementById('searchTerm').onkeyup = (()=>{filterList('searchTerm', "track_table")});
-  document.getElementById('searchButton').onclick = (()=>{filterList('searchTerm', "track_table")});
-
+  document.getElementById('searchTerm').onkeypress = (()=>{filterList('searchTerm')});
+  document.getElementById('searchTerm').onkeyup = (()=>{filterList('searchTerm')});
+  document.getElementById('searchButton').onclick = (()=>{filterList('searchTerm')});
   console.log("Successfully loaded");
 });
 
-function filterList(inputId, tableId){
-  var array = [];
-  var arrayFiltered = [];
-  var words = [];
-  var regex = [];
+function filterList(inputId){
+  var words = document.getElementById(inputId).value.toLowerCase().split(" ");
+  // on enleve les espaces
+  words = words.filter(Boolean);
 
-  var table = document.getElementById(tableId);
-  for (var r = 0, n = table.rows.length; r < n; r++) {
-      for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
-          array.push({row : r, data : table.rows[r].cells[c].innerHTML});
-      }
-  }
-
-  var words = document.getElementById(inputId).value.split(' ');
-  words.forEach((ele)=> {
-    regex.push(new RegExp(ele, 'i'));
-  });
-
-  array.forEach((ele)=> {
-    regex.forEach((re)=> {
-      if (re.test(ele.data)) {
-        arrayFiltered.push(ele.row);
+  jquery(".card").each(function(){
+    let text = jquery(this).prop("title").toLowerCase().split(" ");
+    let found = false;
+    words.forEach(word => {
+      if (text.some(res => res.includes(word))>0){
+        found = true;
       }
     });
-  });
-
-  arrayFiltered = [...new Set(arrayFiltered)];
-
-  for (var r = 0, n = table.rows.length; r < n; r++) {
-    if (!arrayFiltered.includes(r)) {
-      table.rows[r].style="display:none";
-    } else {
-      table.rows[r].style="display:table-row";
+    if (found || words.length == 0 || words == undefined){
+      jquery(this).show();
+    }else{
+     jquery(this).hide();
     }
-  }
+  })
 }
-
 
 /**
 * Same as addTableHTMLBeforeElement but for the database table 'track' only
@@ -74,21 +56,21 @@ function addTrackHTMLBeforeElement(id, tableName) {
       }
 
       let baseLink = "/game?" + encodeQueryData(data);
-
+            
       if (typeof table !== 'undefined' && table.length > 0) {
-        let titles = '';
-        titles = Object.keys(table[0]).join('</th><th>');
-        titles = '<tr><th>'+titles+'</th></tr>';
+        //counter
+        let i = 0;
+        
+        table.forEach((line) => {
+          let title = line.Trackname;
+          let artist = line.Trackartist;
+          let link = "";
+          let cover = "";
 
-        console.log(table[0]);
+          if (tableName == "track"){
+            cover = line.Trackcover;
+          }
 
-        let data = '';
-        let link = '';
-        let cover = '';
-        let lineStr = '';
-
-        table.forEach((line)=>{
-          lineStr = '';
           Object.keys(line).forEach((key)=> {
             if (key==="TrackURI" || key=="Filename") {
               link = baseLink;
@@ -96,20 +78,14 @@ function addTrackHTMLBeforeElement(id, tableName) {
                 link += '&'+encodeURIComponent(key)+"="+encodeURIComponent(line[key]);
               });
             }
-            else if (key==='Trackcover') {
-              cover = line[key];
-            }
-            else {
-              lineStr += '<td>'+line[key]+'</td>';
-            }
           });
 
-          let a1 = "<div class=card><div class=card-image><a href=" + link +"><img src="+ cover + "></a>";
-          a1 += "</div><div class=card-body><div class=card-date><time>" + line["Trackartist"] + "</time></div><div class=card-title>";
-          a1 += "<h2>" + line["Trackname"] + "</h2></div><div class=card-excerpt></div></div></a></div>"
+          let a1 = "<div class=card id="+tableName+"_"+i+"><div class=card-image><a href=" + link +"><img src="+ cover + "></a>";
+          a1 += "</div><div class=card-body><div class=card-date><time>" + artist + "</time></div><div class=card-title>";
+          a1 += "<h2>" + title + "</h2></div><div class=card-excerpt></div></div></a></div>"
 
-          console.log(a1);
-          jquery('#container').append(jquery(a1));
+          jquery('#cards').append(jquery(a1).attr("title", title + " " + artist));
+          i++
         });
       }
     }
